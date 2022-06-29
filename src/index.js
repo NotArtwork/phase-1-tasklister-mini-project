bda001d297460283b4625aa3c22a3daa80f5c48f
NotArtwork <AntArce3@gmail.com>
2022-06-29 11:02:52 -0400

    Done with Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+// const inputForm = document.querySelector('#create-task-form')
+const ul = document.getElementById('tasks')
+const input = document.querySelector('#new-task-description')
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
 });
+
+document.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const li = document.createElement('li');
+  li.textContent = input.value;
+  deleteBtn = document.createElement('button')
+  deleteBtn.textContent = "x"
+  li.append(deleteBtn)
+  ul.append(li)
+  deleteBtn.addEventListener('click', () => {
+    li.remove()
+  });
+}); 
+
+// deleteBtn = document.createElement('button')
+
+// deleteBtn.textContent = "x"
+
+// deleteButton.addEventListener('click', () => {
+// li.remove()
+// });
